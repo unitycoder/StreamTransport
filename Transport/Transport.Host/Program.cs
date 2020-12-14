a50e4f1d23442782c315bb6f4546337f62a5c04f
Fredrik Holmstrom <fredrik.johan.holmstrom@gmail.com>
2020-12-14 19:00:54 +0100

    setup basic project

---
+++ Transport/Transport.Host/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Transport {
+  static class Program {
+    static void Main(string[] args) {
+      Console.WriteLine("Hello World!");
+    }
+  }
+}
---
+++ Transport/Transport/Class1.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+namespace Transport {
+  public class Class1 {
+    
+  }
+}
